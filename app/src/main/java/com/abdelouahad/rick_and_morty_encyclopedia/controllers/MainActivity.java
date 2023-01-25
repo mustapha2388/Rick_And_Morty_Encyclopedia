82b93a0b6e73d94bb9f138b9d65a2696d2e55e9c
mouss <mouss2388@gmail.com>
2023-01-25 14:23:10 +0100

    Add menu in toolbar

--- app/src/main/java/com/abdelouahad/rick_and_morty_encyclopedia/controllers/MainActivity.java
+++ app/src/main/java/com/abdelouahad/rick_and_morty_encyclopedia/controllers/MainActivity.java
@@ -1,10 +1,15 @@
 package com.abdelouahad.rick_and_morty_encyclopedia.controllers;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.abdelouahad.rick_and_morty_encyclopedia.R;
 import com.abdelouahad.rick_and_morty_encyclopedia.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +22,38 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = mBinding.getRoot();
         setContentView(view);
+
+        this.configureToolbar();
+    }
+
+    private void configureToolbar() {
+        setSupportActionBar(mBinding.toolbar.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menu_1:
+                Toast.makeText(this, "click On menu 1", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_2:
+                Toast.makeText(this, "click On menu 2", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_3:
+                Toast.makeText(this, "click On menu 3", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
